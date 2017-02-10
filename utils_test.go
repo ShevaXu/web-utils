000177178c60ea05a0cb740136658c899f7d6a34
Sheva Xu <shevaxu@outlook.com>
2017-02-10 19:46:12 +0800

    test (utils): complete test cases

--- utils_test.go
+++ utils_test.go
@@ -4,9 +4,12 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
+	"fmt"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 	"time"
 
@@ -19,12 +22,17 @@ type testContent struct {
 	Data string `json:"data"`
 }
 
+func addTestHeader(req *http.Request) {
+	req.Header.Add("x-test", "test")
+}
+
 func TestNewJsonPost(t *testing.T) {
-	req, err := NewJsonPost("/", testContent{"hello"})
+	req, err := NewJsonPost("/", testContent{"hello"}, nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 	assert.Equal(t, "application/json; charset=utf-8", req.Header.Get("Content-Type"), "Proper header")
+
 	decoder := json.NewDecoder(req.Body)
 	var c testContent
 	err = decoder.Decode(&c)
@@ -32,6 +40,30 @@ func TestNewJsonPost(t *testing.T) {
 		t.Fatalf("Error decoding the body: %s", err)
 	}
 	assert.Equal(t, c.Data, "hello", `Should respond with "hello"`)
+
+	req, err = NewJsonPost("/", testContent{"hello"}, addTestHeader)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, "test", req.Header.Get("x-test"), "Hooked header")
+}
+
+func TestNewJsonForm(t *testing.T) {
+	v := url.Values{}
+	req, err := NewJsonForm("/", v, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, "", req.Header.Get("Content-Type"), "Proper header")
+
+	body, _ := ioutil.ReadAll(req.Body)
+	assert.Equal(t, v.Encode(), string(body), "Body encoded")
+
+	req, err = NewJsonPost("/", v, addTestHeader)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, "test", req.Header.Get("x-test"), "Hooked header")
 }
 
 type RetryTest struct {
@@ -152,8 +184,8 @@ var Status5xxHandlerFunc = http.HandlerFunc(func(w http.ResponseWriter, r *http.
 
 type retryTest struct {
 	closeTest
-	tries         int
-	expectedTries int
+	tries          int
+	expectedReries int
 }
 
 // TODO: test retry wait/interval setting
@@ -213,7 +245,7 @@ func TestSafeClient_RequestWithRetry(t *testing.T) {
 			assert.Equal(t, test.expectedCode, status, "Returns code")
 			assert.Equal(t, test.expectedBody, body, "Returns body")
 		}
-		assert.Equal(t, test.expectedTries, n, "Report retried times")
+		assert.Equal(t, test.expectedReries, n, "Report retried times")
 
 		server.Close()
 	}
@@ -235,8 +267,19 @@ func TestSafeClient_RequestWithRetry_Bug(t *testing.T) {
 
 	_, _, _, err = testTimeoutClient.RequestWithRetry(req, 3)
 
+	//fmt.Println(err) // Post http://127.0.0.1:49833: http: ContentLength=3 with Body length 0
 	assert.True(t, err != nil, "Should have error")
 }
+
+var CheckHeaderHandlerFunc = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("x-test") == "test" {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	} else {
+		w.WriteHeader(http.StatusForbidden)
+		w.Write([]byte("Wrong header"))
+	}
+})
 
 func TestSafeClient_PostJsonWithRetry(t *testing.T) {
 	tests := []retryTest{
@@ -270,11 +313,21 @@ func TestSafeClient_PostJsonWithRetry(t *testing.T) {
 			5,
 			4,
 		},
+		{
+			closeTest{
+				CheckHeaderHandlerFunc,
+				http.StatusForbidden,
+				[]byte("Wrong header"),
+				false,
+			},
+			5,
+			0, // no retry if forbidden
+		},
 	}
 
 	for _, test := range tests {
 		server := httptest.NewServer(test.h)
-		n, status, body, err := testTimeoutClient.PostJsonWithRetry(server.URL, testContent{"foo"}, test.tries)
+		n, status, body, err := testTimeoutClient.PostJsonWithRetry(server.URL, testContent{"foo"}, test.tries, nil)
 		if test.expectTimeout {
 			if err != nil {
 				assert.Equal(t, true, IsTimeoutErr(err), "Should be")
@@ -288,8 +341,109 @@ func TestSafeClient_PostJsonWithRetry(t *testing.T) {
 			assert.Equal(t, test.expectedCode, status, "Returns code")
 			assert.Equal(t, test.expectedBody, body, "Returns body")
 		}
-		assert.Equal(t, test.expectedTries, n, "Report retried times")
+		assert.Equal(t, test.expectedReries, n, "Report retried times")
 
 		server.Close()
 	}
+
+	// hooked case
+	server := httptest.NewServer(CheckHeaderHandlerFunc)
+	n, status, _, err := testTimeoutClient.PostJsonWithRetry(server.URL, testContent{"foo"}, 3, addTestHeader)
+	if err != nil {
+		t.Errorf("Error request: %s", err)
+	}
+	assert.Equal(t, http.StatusOK, status, "Returns code")
+	assert.Equal(t, 0, n, "Report retried times")
+
+	server.Close()
+}
+
+func TestSafeClient_PostFormWithRetry(t *testing.T) {
+	tests := []retryTest{
+		{
+			closeTest{
+				OkHandlerFunc,
+				http.StatusOK,
+				[]byte("OK"),
+				false,
+			},
+			3,
+			0,
+		},
+		{
+			closeTest{
+				TimeoutHandlerFunc,
+				http.StatusOK,
+				nil,
+				true,
+			},
+			3,
+			2,
+		},
+		{
+			closeTest{
+				Status5xxHandlerFunc,
+				http.StatusInternalServerError,
+				[]byte(internalErr),
+				false,
+			},
+			5,
+			4,
+		},
+		{
+			closeTest{
+				CheckHeaderHandlerFunc,
+				http.StatusForbidden,
+				[]byte("Wrong header"),
+				false,
+			},
+			5,
+			0, // no retry if forbidden
+		},
+	}
+
+	for _, test := range tests {
+		server := httptest.NewServer(test.h)
+		n, status, body, err := testTimeoutClient.PostFormWithRetry(server.URL, url.Values{}, test.tries, nil)
+		if test.expectTimeout {
+			if err != nil {
+				assert.Equal(t, true, IsTimeoutErr(err), "Should be")
+			} else {
+				t.Error("Should return timeout error")
+			}
+		} else {
+			if err != nil {
+				t.Errorf("Error request: %s", err)
+			}
+			assert.Equal(t, test.expectedCode, status, "Returns code")
+			assert.Equal(t, test.expectedBody, body, "Returns body")
+		}
+		assert.Equal(t, test.expectedReries, n, "Report retried times")
+
+		server.Close()
+	}
+
+	// hooked case
+	server := httptest.NewServer(CheckHeaderHandlerFunc)
+	n, status, _, err := testTimeoutClient.PostFormWithRetry(server.URL, url.Values{}, 3, addTestHeader)
+	if err != nil {
+		t.Errorf("Error request: %s", err)
+	}
+	assert.Equal(t, http.StatusOK, status, "Returns code")
+	assert.Equal(t, 0, n, "Report retried times")
+
+	server.Close()
+}
+
+func TestStdClient(t *testing.T) {
+	cl := StdClient()
+	assert.NotNil(t, cl, "StdClient not nil")
+	addr := fmt.Sprintf("%p", cl)
+
+	cl2 := StdClient()
+	addr2 := fmt.Sprintf("%p", cl2)
+
+	assert.Equal(t, cl, cl2, "Every call returns a value-equal client")
+	//fmt.Println(addr, addr2)
+	assert.NotEqual(t, addr, addr2, "Every call returns a defferent client")
 }
